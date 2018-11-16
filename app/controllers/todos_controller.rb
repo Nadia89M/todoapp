e574b9a21786dbf211385ae56aa6a2a79d44c115
Nadia <nadia.mohamed89@gmail.com>
2018-11-16 17:05:02 +0000

    CRUD created

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,53 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @todos = Todo.all
+  end
+
+  def show
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      flash[:notice] = "Todo was succesfully created!"
+      redirect_to todo_path(@todo)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @todo.update(todo_params)
+      flash[:notice] = "Todo was succesfully updated!"
+      redirect_to todo_path(@todo)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @todo.destroy
+    flash[:notice] = "Todo was succesfully deleted!"
+    redirect_to todos_path
+  end
+
+
+  private
+
+  def set_todo
+      @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:name, :description)
+  end
+end
